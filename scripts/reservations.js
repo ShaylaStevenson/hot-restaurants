8f333a8d56e261785f5b8eee82cd3805bbcaaebc
Conor O'Brien <obrienceob@gmail.com>
2021-03-04 20:20:27 -0800

    adding in reservation and table arrays

---
+++ scripts/reservations.js
@@ -0,0 +1,10 @@
+const reservationArray = [
+    {
+      customerName: 'Matache',
+      customerEmail: 'matache@mail.com',
+      customerID: '11234542',
+      phoneNumber: '555-555-5555',
+    },
+  ];
+
+module.exports = reservationArray
