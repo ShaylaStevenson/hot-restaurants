6ddbb6af637e965357c9c9662edd1d33aa0105af
Conor O'Brien <obrienceob@gmail.com>
2021-03-04 20:13:21 -0800

    corrected server.js

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const express = require('express');
-const path = require('path')
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-
+require('./routes/apiRoutes')(app);
+require('./routes/htmlRoutes')(app);
 
 
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
